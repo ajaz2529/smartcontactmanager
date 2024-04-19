0f2a34f7b0f8055814f6e6cc2a830a1b5df2499b
AJAZ BHAT <ajazbhat2529@gmail.com>
2024-04-19 17:49:40 +0530

    intial

---
+++ src/main/java/com/smart/controller/singleton.java
@@ -0,0 +1,18 @@
+package com.smart.controller;
+
+public class singleton {
+
+    private static singleton instance;
+
+    private singleton(){}
+
+
+    public static singleton getInstance(){
+        if(instance==null){
+            instance = new singleton();
+        }
+        return instance;
+    }
+
+
+}
